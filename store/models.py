e49a1d8489dede2e4c31c99391afd2222b79b71d
medvedevmr <medvedevmr.work@gmail.com>
2024-01-10 17:48:32 +0100

    PreMigration Stage

--- store/models.py
+++ store/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
+    
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=20)
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+    
+    
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.DecimalField(default=0, max_digits=6, decimal_places=2)
+    category = models.ForeignKey(Category, default=1, on_delete=models.CASCADE)
+    description = models.CharField(max_length=300, default='', blank=True, null=True)
+    image = models.ImageField(upload_to='uploads/product/')
+    
+    def __str__(self):
+        return self.name
+    
+
+class Order(models.Model):
+    
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    address = models.CharField(max_length=100, default='', blank=True)
+    phone = models.CharField(max_length=20, default='', blank=True)
+    date = models.DateField(default=datetime.datetime.today)
+    ststus = models.BooleanField(default=False)
+        
+    
+    def __str__(self):
+        return self.product
