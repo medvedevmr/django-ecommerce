f807be76649190efcd02182fb743f8a1a9045ab2
medvedevmr <medvedevmr.work@gmail.com>
2024-01-17 17:22:49 +0100

    Caterories Page Added

--- store/views.py
+++ store/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Product
+from .models import Product, Category
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
@@ -7,6 +7,21 @@ from django.contrib.auth.forms import UserCreationForm
 from .forms import SignUpForm
 from django import forms
 
+
+def category(request,cat):
+    cat = cat.replace('-',' ')
+    try:
+        category = Category.objects.get(name=cat)
+        products = Product.objects.filter(category=category)
+        return render(request, 'category.html', {'products':products, 'category':category})
+    except:
+        messages.success(request,("That Category Doesn't Exist"))
+        return redirect('home')
+    return render(request, 'category.html', {'category':category})
+
+def product(request,pk):
+    product = Product.objects.get(id=pk)
+    return render(request, 'product.html', {'product':product})
 
 def home(request):
     products = Product.objects.all()
