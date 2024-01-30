2354a27ac58084e6912c0ada4e52a284200800f6
medvedevmr <medvedevmr.work@gmail.com>
2024-01-30 13:34:57 +0100

    Updating User Info Has Been Added

--- store/views.py
+++ store/views.py
@@ -4,8 +4,44 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .forms import SignUpForm
+from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm
 from django import forms
+
+def update_password(request):
+    if request.user.is_authenticated:
+        current_user = request.user
+        if request.method == 'POST':
+            form = ChangePasswordForm(current_user,request.POST)
+            if form.is_valid():
+                form.save()
+                messages.success(request,("Your Password Has Been Changed"))
+                login(request,current_user)
+                return redirect('login')
+            else:
+                for error in list(form.errors.values()):
+                    messages.error(request,error)
+                return redirect('update_password')
+        else:
+            form = ChangePasswordForm(current_user)
+            return render(request, 'update_password.html', {'form':form})
+    else:
+        messages.success(request,("You Must Be Logged In"))
+        return redirect('home')
+
+def update_user(request):
+    if request.user.is_authenticated:
+        current_user = User.objects.get(id=request.user.id)
+        user_form = UpdateUserForm(request.POST or None, instance=current_user)
+        
+        if user_form.is_valid():
+            user_form.save()
+            login(request,current_user)
+            messages.success(request,("User Info Updated"))
+            return redirect('home')
+        return render(request, 'update_user.html', {'user_form':user_form})
+    else:
+        messages.success(request,("You Must Be Logged In"))
+        return redirect('home')
 
 def category_summary(request):
     categories = Category.objects.all()
