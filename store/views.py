1e35c8cf20b3cfa5195b9a71f46bbacf8fd919cc
medvedevmr <medvedevmr.work@gmail.com>
2024-02-18 16:52:31 +0100

    Search Added

--- store/views.py
+++ store/views.py
@@ -6,6 +6,19 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm, UserInfoForm 
 from django import forms
+from django.db.models import Q
+
+def search(request):
+	if request.method == "POST":
+		searched = request.POST['searched']
+		searched = Product.objects.filter(Q(name__icontains=searched) | Q(description__icontains=searched))
+		if not searched:
+			messages.success(request, "That Product Does Not Exist...Please try Again.")
+			return render(request, "search.html", {})
+		else:
+			return render(request, "search.html", {'searched':searched})
+	else:
+		return render(request, "search.html", {})
 
 def update_info(request):
     if request.user.is_authenticated:
